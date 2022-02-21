4bae70ad3f5c017cbdf44af4da0402b3d364ff15
Home <Home@HOMEPC>
2022-02-21 20:46:26 +0300

    Add project files.

---
+++ Nedra/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Nedra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Path\Input.txt";// переменная указывающая путь до файла
+            string[] lines = File.ReadAllLines(path);// считываем весь файл в масситв строк
+            int[][] num = new int[lines.Length][];//объявляем зубчатый массив
+            for (int i = 0; i < lines.Length; i++)// цикл для переноса и преобразования значений из файла в массив
+            {
+                string[] temp = lines[i].Split(' ');// каждую строкуб преобразовываем в массив. Значения рпазделены пробелом
+                num[i] = new int[temp.Length]; // для каждой новой строки выделяем выделяем память
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (temp[j] != null)// проверка не конец ли строки
+                        num[i][j] = Convert.ToInt32(temp[j]);// если нет: заносим в массив
+                    else
+                        break;//если да: переходим к следующей итерации
+                }
+            }
+            method ans = new method(num); //создаем объект класса метод и передаем ему массив как параметр 
+        }
+    }    
+}
---
+++ Nedra/method.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedra
+{
+    internal class method
+    {
+        public method(int[][] ar)//создаём конструктор класса
+        {
+            Console.WriteLine(big_sum(ar));// выводим на консоль результат работы метода big_sum
+        }
+        /// <summary>
+        /// Метод котрый выводит найти максимальную сумму, которую можно получить из различных маршрутов прохождения по массиву
+        /// </summary>
+        /// <param name="array"> обязательный паркметр, массив в котором будет идти поитск</param>
+        /// <param name="row">необязательный параметр,начльное знаение =0; указывает на каком уровне сейчас метод</param>
+        /// <param name="column">необязательный параметр,начльное знаение =0; указывает на каком ряду сейчас метод</param>
+        /// <param name="total">необязательный параметр,начльное знаение =0; указывает максимальнрую сумму для конкретных row и column</param>
+        /// <returns></returns>
+        int big_sum(int[][] array, int row = 0, int column = 0, int total = 0)
+        {
+            if (row == array.GetLength(0) - 1)// проверка: а не на последним ли уровне
+            {
+                return total + array[row][column];//если да, то просто возвращаем значение total+ значение на нижнем уровне
+            }
+            // если же не на последнем уровне, тогда необходимо идти по пути наибольшей суммы
+            // поэтому проверяем для значений row и column значения которые находятся на уровень ниже  и соседнее с ним
+            // и возвроащем значение наибольшего + total(то есть, суммы всех значений до этого уровня)
+            if (big_sum(array, row + 1, column, total + array[row][column]) >
+                   big_sum(array, row + 1, column + 1, total + array[row][column]))
+            {
+                return big_sum(array, row + 1, column, total + array[row][column]);
+            }
+            else
+            {
+                return big_sum(array, row + 1, column + 1, total + array[row][column]);
+            }
+        }
+    }
+}
